f9aa1535ba86dc71b655ba21d7a170197d1abc67
xxd <xuexiaodong79@gmail.com>
2014-09-22 09:38:39 +0800

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   	#@user = User.new(params[:user]) #Rails之前版本使用，意思是@user = User.new(name: "Foo Bar", email: "foo@invalid", password: "foo", password_confirmation: "bar")
   	@user = User.new(user_params) #Rails4以后的新写法
   	if @user.save
-       flash[:success] = "Welcome to the Sample App!"
+       flash[:success] = "欢迎来到痣上。。。。"
   		 redirect_to @user
   	else
   		 render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ActiveRecord::Base
-	#这两个字断不能为空，name长度不能超过50，email格式验证
-	validates :name,  presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false } #Rails 会自动指定 :uniqueness 的值为 true
-    before_save { self.email = email.downcase }
-    has_secure_password
-    validates :password, length: { minimum: 6 }
+  #这两个字断不能为空，name长度不能超过50，email格式验证
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                  format: { with: VALID_EMAIL_REGEX },
+                  uniqueness: { case_sensitive: false } #Rails 会自动指定 :uniqueness 的值为 true
+  before_save { self.email = email.downcase }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+  before_create :create_remember_token
+
+  def User.new_remember_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
