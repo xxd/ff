19270ec79329ed4f21d3f89fa0fea5c2bb7a067b
xxd <xuexiaodong79@gmail.com>
2014-11-24 14:32:18 +0800

    added: API

--- app/controllers/sessions_controller_20141118165712.rb
+++ app/controllers/sessions_controller_20141118165712.rb
@@ -1,24 +1,21 @@
 class SessionsController < ApplicationController
-  # respond_to :html, :json
+  respond_to :html, :json
+
   def new
   end
 
   def create
 #    user = User.find_by(email: params[:session][:email].downcase) || User.find_by(name: params[:session][:name].downcase) #尝试username和email登陆不行
 	  user = User.find_by(email: params[:session][:email].downcase)
-    @user = User.find_by(email: params[:session][:email].downcase)
+    puts params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      # redirect_to user #只返回html
-      respond_to do |format|
-        format.html { redirect_to @user } 
-        # format.json { render json: @user } #返回全部@user
-        format.json { render json: @user.as_json(  only: [ :id, :name, :email ] ) }
-      end  
-      # respond_with(@user) #也可以，但是是返回全部
+      #format.html { redirect_to user }
+      format.json @user
     else
       flash.now[:error] = 'Email或者密码错误' # Not quite right!
-      render 'new'
+      #render 'new'
+      render json: 0
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ SampleApp::Application.routes.draw do
   resources :relationships, only: [:create, :destroy]
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
-  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signin',  to: 'sessions#create',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
